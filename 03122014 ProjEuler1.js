d77e49c93af3a914e3e8650790e6c32f4c69ad98
Dara Mao <maodara@gmail.com>
2014-03-12 21:10:35 -0400

    project euler 1

---
+++ 03122014 ProjEuler1.js
@@ -0,0 +1,18 @@
+/* 
+Author: Dara Mao
+Date: 03-12-2014
+Project: To solve Project Euler #1 (Find the sum of all the multiples of 3 or 5 below 1000.)
+*/
+
+function numbers (){
+	var sum = 0;
+	for (i = 0; i < 1000; i++){
+	if (i % 3 == 0 || i % 5 == 0){
+	sum += i;
+	}
+	}
+	document.write(sum);
+	}
+	
+	numbers ();
+	
