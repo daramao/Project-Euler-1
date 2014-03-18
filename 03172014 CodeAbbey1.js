7931279f76f8778f62286eef027fff4b230cec3f
Dara Mao <maodara@gmail.com>
2014-03-18 09:56:57 -0400

    CodeAbbey

---
+++ 03172014 CodeAbbey1.js
@@ -0,0 +1,12 @@
+/*
+CodeAbbey
+Author: Dara Mao
+Date: 03-17-2014
+Project: Working through CodeAbbey challenges #1-
+*/
+
+//Challenge 1: Sum "A+B"
+var a=1965388,b=2891413;
+alert(a+b);
+
+//Challenge 2: Sum in Loop
