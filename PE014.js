473a93814742a82603df5182b942501d3ab330a3
daramao <dara.mao@gmail.com>
2014-08-09 13:47:42 -0400

    Refactoring # 14

--- PE014.js
+++ PE014.js
@@ -1,39 +1,39 @@
 // PE # 14 : Longest Collatz sequence
 // 08/07/2014
 
+function baseCond(x){
+    if (x % 2 === 0){
+        x = x / 2;
+    } else  {
+       x = 3 * x + 1;
+    }
+    return x;
+}
 
 function CollatzSeq(n) {
-
-    arrayLength = [];
 
     for (var i= n; i >= 1;i--){
         x = i;
         var array = [x];
-
-        function baseCond (x){
-            if (x % 2 === 0){
-                x = x / 2;
-            } else  {
-                 x = 3 * x + 1;
-            }
-        }
-
         array.push(i+"hi");
 
+        baseCond(x);
+
         while (x > 1 ){
-//Need to refactor 08/08/2014
             if (x % 2 === 0){
                 x = x / 2;
             } else  {
-                 x = 3 * x + 1;
+               x = 3 * x + 1;
             }
-            array.push(x);
+        array.push(x);
         }
 
-    length = array.length;
-    arrayLength.push(length);
-    var max = Math.max.apply(Math, arrayLength);
-    console.log(max);
-    }
+        arrayLength = [];
+        length = array.length;
+        arrayLength.push(length);
+        var max = Math.max.apply(Math, arrayLength);
+        console.log(max);
+        }
 }
+
 CollatzSeq(1000000);
